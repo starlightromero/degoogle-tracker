bba67eb605aecd5184aa6142c4743bc66a8698fd
Starlight Romero <starlightromero@protonmail.com>
2021-02-26 10:46:17 -0800

    [update] google form

---
+++ degoogle_app/google/forms.py
@@ -0,0 +1,11 @@
+"""Google forms."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+
+class GoogleForm(FlaskForm):
+    name = StringField(
+        "Google Service", validators=[DataRequired(), Length(min=3, max=50)]
+    )
+    submit = SubmitField("Add")
