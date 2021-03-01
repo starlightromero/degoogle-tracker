83ae3d84bf5ac70dc13e65394482c0aef65914b5
Starlight Romero <starlightromero@protonmail.com>
2021-03-01 11:31:21 -0800

    [update]removed update alternative

--- degoogle_app/alternative/routes.py
+++ degoogle_app/alternative/routes.py
@@ -2,52 +2,43 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
 from degoogle_app.alternative.forms import AlternativeForm
-from degoogle_app.models import Alternative
+from degoogle_app.models import Alternative, Google
 from degoogle_app import db
 
 alternative = Blueprint("alternative", __name__)
 
 
-@login_required
 @alternative.route("/", methods=["GET"])
-def get_alternative_services():
-    """Get current user's alternative services."""
+@login_required
+def get_alternative_software():
+    """Get current user's alternative software."""
     form = AlternativeForm()
     return render_template("alternative.html.j2", form=form)
 
 
-@login_required
 @alternative.route("/", methods=["POST"])
-def add_alternative_services():
-    """Add new alternative service to current user."""
+@login_required
+def add_alternative_software():
+    """Add new alternative software to current user."""
     form = AlternativeForm()
     if form.validate_on_submit:
-        alternative = Alternative(name=form.name.data)
+        alternative = Alternative(
+            name=form.name.data,
+            user_id=current_user.id,
+            google_id=form.alternative.data.id,
+        )
         db.session.add(alternative)
         db.session.commit()
-        flash("The alternative service has been added")
-    return redirect(url_for("alternative.get_alternative_services"))
+        flash("The alternative software has been added")
+    return redirect(url_for("alternative.get_alternative_software"))
 
 
+@alternative.route("/<software_id>", methods=["DELETE"])
 @login_required
-@alternative.route("/:id", methods=["PATCH"])
-def update_alternative_services(id):
-    """Update alternative service for current user."""
-    alternative = Alternative.query.get_or_404(id)
-    form = AlternativeForm()
-    if form.validate_on_submit:
-        alternative.name = form.name.data
-        db.session.commit()
-        flash("The alternative service has been updated.")
-    return redirect(url_for("alternative.get_alternative_services"))
-
-
-@login_required
-@alternative.route("/:id", methods=["DELETE"])
-def delete_alternative_services(id):
-    """Delete alternative service for current user."""
-    alternative = Alternative.query.get_or_404(id)
+def delete_alternative_software(software_id):
+    """Delete alternative software for current user."""
+    alternative = Alternative.query.get_or_404(software_id)
     db.session.delete(alternative)
     db.session.commit()
     flash(f"{alternative.name} has been deleted.")
-    return redirect(url_for("alternative.get_alternative_services"))
+    return redirect(url_for("alternative.get_alternative_software"))
