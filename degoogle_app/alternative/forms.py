2ac440f8154440ea0ad9f82a5f639040859db3cc
Starlight Romero <starlightromero@protonmail.com>
2021-03-01 11:30:56 -0800

    [update] AlternativeForm to only have alternatives for the current user

--- degoogle_app/alternative/forms.py
+++ degoogle_app/alternative/forms.py
@@ -1,5 +1,6 @@
 """Alternative forms."""
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
@@ -8,12 +9,15 @@ from degoogle_app.models import Google
 
 class AlternativeForm(FlaskForm):
     name = StringField(
-        "Alternative Service",
+        "Alternative Software",
         validators=[DataRequired(), Length(min=3, max=50)],
     )
     alternative = QuerySelectField(
         "Alternative To",
-        query_factory=lambda: Google.query,
+        query_factory=lambda: Google.query.filter(
+            Google.user_id == current_user.id
+        ).order_by(Google.name),
         allow_blank=False,
+        validators=[DataRequired()],
     )
     submit = SubmitField("Add")
